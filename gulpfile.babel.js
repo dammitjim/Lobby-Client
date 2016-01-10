eef68ae1793cdadea1bf31e59dcf443f88c7d9f0
Jim Hill <jimi2204@googlemail.com>
2016-01-10 03:21:14 +0000

    Moving over to scss as its a bit more sane, adding scss linter to build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-ruby-sass');
 
 gulp.task('compile_styles', () => {
-  return sass('./src/front-end/styles/styles.sass')
+  return sass('./src/front-end/styles/styles.scss')
     .pipe(gulp.dest('./src/front-end/styles/'));
 });
 
 gulp.task('default', ['compile_styles'], () => {
-  gulp.watch('./src/front-end/styles/**/*.sass', () => {
+  gulp.watch('./src/front-end/styles/**/*.scss', () => {
     gulp.run('compile_styles');
   });
 });
